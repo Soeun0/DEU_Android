f2a831cd85f176948d3915d76d275ab095462db4
Soeun0 <144868086+Soeun0@users.noreply.github.com>
2024-10-24 21:25:03 +0900

    TodoList
    초기버전입니다.

---
+++ LiteToDo-v0.1/app/src/main/java/com/sangkon/litetodo/TodoAdapter.kt
@@ -0,0 +1,36 @@
+package com.sangkon.litetodo
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.sangkon.litetodo.databinding.TodoCardViewBinding
+
+
+/*홀더에 있는 친구가 cardview로 변경해주는 어뎁터*/
+class TodoAdapter :RecyclerView.Adapter<TodoAdapter.TodoHolder>(){
+
+    private val todoList: MutableList<Todo> = mutableListOf()
+
+    override fun getItemCount(): Int = todoList.size
+
+    class TodoHolder(private val binding: TodoCardViewBinding):
+        RecyclerView.ViewHolder(binding.root) {
+        fun dinDate(todo: Todo) {
+            binding.todo = todo
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType:Int): TodoHolder {
+        val binding =
+            TodoCardViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TodoHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TodoHolder, position: Int) {
+        holder.dinDate(todoList[position])
+    }
+
+    fun setTodoList(list: List<Todo>) {
+        todoList.addAll(list)
+    }
+}
